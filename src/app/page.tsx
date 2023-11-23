66f5e03d65edc11ec07f8ac79c7b7a0106675d42
Shirahamah <hiropi1224s@gmail.com>
2023-11-23 13:54:59 +0900

    ♻️ TremorCard修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Text, Metric, Flex, ProgressBar } from '@tremor/react';
+
 import { TremorCard } from '@/components/tremor/tremor-card';
 import { Button } from '@/components/ui/button';
 
@@ -5,7 +7,15 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       Hello, world!
-      <TremorCard />
+      <TremorCard className='mx-auto max-w-xs rounded'>
+        <Text>Sales</Text>
+        <Metric>$ 71,465</Metric>
+        <Flex className='mt-4'>
+          <Text>32% of annual target</Text>
+          <Text>$ 225,000</Text>
+        </Flex>
+        <ProgressBar value={32} className='mt-2' />
+      </TremorCard>
       <Button variant='destructive'>Button</Button>
     </main>
   );
--- src/components/tremor/tremor-card.tsx
+++ src/components/tremor/tremor-card.tsx
@@ -1,15 +1,12 @@
-import { Card, Text, Flex, Metric, ProgressBar } from '@tremor/react';
+import { Card } from '@tremor/react';
+import { PropsWithChildren } from 'react';
 
-export const TremorCard: React.FC = () => {
-  return (
-    <Card className='mx-auto max-w-xs rounded'>
-      <Text>Sales</Text>
-      <Metric>$ 71,465</Metric>
-      <Flex className='mt-4'>
-        <Text>32% of annual target</Text>
-        <Text>$ 225,000</Text>
-      </Flex>
-      <ProgressBar value={32} className='mt-2' />
-    </Card>
-  );
+type CardProps = {
+  className?: string;
+};
+
+type Props = CardProps & PropsWithChildren;
+
+export const TremorCard: React.FC<Props> = ({ children, ...props }) => {
+  return <Card className={props.className}>{children}</Card>;
 };
